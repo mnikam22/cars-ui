2f12546bf1b35d1a9a1cef423e2c3acf651ebe54
tusharncuelogic <tushar.nikam@cuelogic.co.in>
2018-01-11 19:08:07 +0530

    Changed theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { 
   title = 'app';
+  constructor(private renderer : Renderer2){
+    this.renderer.addClass(document.body, 'm-home');
+  }
 }
